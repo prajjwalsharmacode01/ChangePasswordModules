d37874d260976fdfbacc768b122bf0316965d051
Prajjwal Sharma <prajjwal.sharma@paynways.com>
2026-01-03 15:58:58 +0530

    code

---
+++ src/main/java/com/example/changepassword/ChangepasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.changepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangepasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangepasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/changepassword/ChangepasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.changepassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangepasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
